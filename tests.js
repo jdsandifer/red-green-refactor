0215d80cac56bcb993a342fd25a711a952aa51e3
jdsandifer <sandifer.jd@gmail.com>
2018-07-19 13:22:22 -0700

    Add (failing) test = Red

--- tests.js
+++ tests.js
@@ -8,3 +8,12 @@ QUnit.test( "When no one is there...", function( assert ) {
         "Use any one except #5 (1-4)."
     )
 });
+QUnit.test( "When someone is at #3...", function( assert ) {
+    const situation = [false, false, true, false, false]
+
+    assert.deepEqual( 
+        urinalToUse(situation),
+        [1], 
+        "Use #1."
+    )
+});
