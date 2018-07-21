06e92b8ea9dd48c7e97c0831c39897b19becd3bd
jdsandifer <sandifer.jd@gmail.com>
2018-07-21 14:19:44 -0700

    Make it green (add enough code to pass test)

--- urinal-selection.js
+++ urinal-selection.js
@@ -4,6 +4,7 @@
 // isOccupied is an array of flags (booleans) - true means occupied.
 // Indeces represent urinals starting at the one nearest the door.
 const urinalToUse = isOccupied => {
+
     // Add a zeroeth item to the array so we can use
     // indeces as if they started at one and
     // make it a different type so it's easier to
@@ -14,6 +15,8 @@ const urinalToUse = isOccupied => {
         return [1]
     } else if (isOccupied[1]) {
         return [3]
+    } else if (isOccupied[5]) {
+        return [1]
     }
     return [1, 2, 3, 4]
 }
