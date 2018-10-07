a83cf1dbbb2a57eaa3f6e2eda3751602e42d5e7a
J.D. Sandifer <sandifer.jd@gmail.com>
2018-10-06 22:03:40 -0700

    Move more code into helper functions

--- urinal-selection.js
+++ urinal-selection.js
@@ -1,67 +1,87 @@
 // See "Proper Urinal Etiquette" video to understand this algorithm.
 const urinalToUse = isOccupied => {
-  // TODO: Ensure isOccupied has 35 or fewer booleans and at least one is false
-  const urinalSituation = isOccupied.map(urinalSituationFromBooleanArray).join("")
-  return urinalChoice(urinalSituation)
+    // TODO: Ensure isOccupied has 35 or fewer booleans and at least one is false
+    const urinalSituation = isOccupied.map(urinalSituationFromBooleanArray).join("")
+    return urinalChoice(urinalSituation)
 }
 
 
 const urinalSituationFromBooleanArray = (isTaken, index) => {
-  const unoccupiedDigit = "."
-  const oneBasedIndex = index + 1
-  const baseThirtySixDigitForOccupied = 
-      oneBasedIndex <= 9 ? oneBasedIndex : String.fromCharCode(oneBasedIndex + 87)
-  
-  return isTaken ? baseThirtySixDigitForOccupied : unoccupiedDigit
+    const unoccupiedDigit = "."
+    const oneBasedIndex = index + 1
+    const baseThirtySixDigitForOccupied = 
+        oneBasedIndex <= 9 ? oneBasedIndex : String.fromCharCode(oneBasedIndex + 87)
+    
+    return isTaken ? baseThirtySixDigitForOccupied : unoccupiedDigit
 }
 
 
 const urinalChoice = urinalSituation => {
     const allUrinalsAreOpen = [].every.call(urinalSituation, digit => digit === ".")
     if (allUrinalsAreOpen) {
-        let tempArray = new Array(urinalSituation.length - 1)
-        tempArray.fill(1)
-        const allUrinalsButLast = tempArray.map((_, index) => index + 1)
-        return allUrinalsButLast
+        const oneLessThanNumberOfUrinals = urinalSituation.length - 1
+        const anyUrinalButLast = oneBasedIndexArrayWithLength(oneLessThanNumberOfUrinals)
+        return anyUrinalButLast
     }
 
-    if (matchesPattern("1*.4.", urinalSituation)
-        || matchesPattern("*2..5", urinalSituation)) {
+    if (urinalSituation.length = 5) {
+        return urinalChoiceFromVideo(urinalSituation)
+    }
+
+    return urinalChoiceInOtherCases(urinalSituation)
+}
+
+
+const urinalChoiceFromVideo = situation => {
+    if (matchesPattern("1*.4.", situation)
+        || matchesPattern("*2..5", situation)) {
         return [3]
     }
 
-    if (matchesPattern(".2.4.", urinalSituation)) {
+    if (matchesPattern(".2.4.", situation)) {
         return [1]
-    } else if (matchesPattern("1.3.5", urinalSituation)
-               || matchesPattern("...4*", urinalSituation)) {
+    } else if (matchesPattern("1.3.5", situation)
+               || matchesPattern("...4*", situation)) {
         return [2]
     }
 
-    if (matchesPattern(".23..", urinalSituation)) {
+    if (matchesPattern(".23..", situation)) {
         return [1, 5]
     }
 
-    const urinalFreeOfNeighbors = firstOpenSlotWithoutNeighbors(urinalSituation)
+    return urinalChoiceInOtherCases(situation)
+}
+
+
+const urinalChoiceInOtherCases = situation => {
+    const urinalFreeOfNeighbors = firstOpenSlotWithoutNeighbors(situation)
     if (urinalFreeOfNeighbors != -1) {
         return [urinalFreeOfNeighbors]
     }
 
-    const firstOpenUrinal = urinalSituation.indexOf(".") + 1
+    const firstOpenUrinal = situation.indexOf(".") + 1
     return [firstOpenUrinal]
 }
 
 
+const oneBasedIndexArrayWithLength = length => {
+    let tempArray = new Array(length)
+    tempArray.fill(1)
+    return tempArray.map((_, index) => index + 1)
+}
+
+
 const matchesPattern = (pattern, situation) => {
-  for (let i = 1; i <= pattern.length; i++) {
-    if (pattern.charAt(i-1) === "*") {
-      continue
+    for (let i = 1; i <= pattern.length; i++) {
+        if (pattern.charAt(i-1) === "*") {
+            continue
+        }
+        if (pattern.charAt(i-1) === situation.charAt(i-1)) {
+            continue
+        }
+        return false
     }
-    if (pattern.charAt(i-1) === situation.charAt(i-1)) {
-      continue
-    }
-    return false
-  }
-  return true
+    return true
 }
 
 
